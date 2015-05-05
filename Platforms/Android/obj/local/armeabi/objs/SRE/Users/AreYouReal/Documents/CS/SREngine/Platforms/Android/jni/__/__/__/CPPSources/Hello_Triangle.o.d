74f14178a97eaa7c502120c77d7cb0eb7febf7d7
Alexander <viertual@gmail.com>
2015-05-05 16:05:23 +0300

    Repo added.

---
+++ Platforms/Android/obj/local/armeabi/objs/SRE/Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/jni/__/__/__/CPPSources/Hello_Triangle.o.d
@@ -0,0 +1,95 @@
+/Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/obj/local/armeabi/objs/SRE//Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/jni/__/__/__/CPPSources/Hello_Triangle.o: \
+ /Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/jni/../../../CPPSources/Hello_Triangle.c \
+ /Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/jni/../../../Common/Include/esUtil.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/stdlib.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/cdefs.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/api-level.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/string.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/malloc.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/alloca.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/strings.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/stdint.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/_types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/machine/_types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/posix_types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/stddef.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/compiler.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/asm/posix_types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/asm/types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/types.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/machine/kernel.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/memory.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/GLES3/gl3.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/GLES3/gl3platform.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/KHR/khrplatform.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/egl.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/eglplatform.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/native_window.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/rect.h \
+ /Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/eglext.h
+
+/Users/AreYouReal/Documents/CS/SREngine/Platforms/Android/jni/../../../Common/Include/esUtil.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/stdlib.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/api-level.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/string.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/malloc.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/alloca.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/strings.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/stdint.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/_types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/machine/_types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/stddef.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/compiler.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/asm/types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/linux/types.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/machine/kernel.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/memory.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/GLES3/gl3.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/GLES3/gl3platform.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/egl.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/eglplatform.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/native_window.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/android/rect.h:
+
+/Users/AreYouReal/Documents/SOK/android-ndk-r10d/platforms/android-18/arch-arm/usr/include/EGL/eglext.h:
