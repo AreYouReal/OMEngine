f3382b5e83365aa32c35305eb68f27ad9a2ee344
Alexander <viertual@gmail.com>
2016-01-22 11:58:44 +0300

    Android build.

--- Platforms/Android/obj/local/armeabi/objs/OME/Users/AreYouReal/Documents/Other/OMEngine/Platforms/Android/jni/__/__/__/CPPSources/Utils/3dPart/BulletPhysics/LinearMath/btSerializer.o.d
+++
@@ -1,2 +0,0 @@
-/Users/AreYouReal/Documents/Other/OMEngine/Platforms/Android/obj/local/armeabi/objs/OME//Users/AreYouReal/Documents/Other/OMEngine/Platforms/Android/jni/__/__/__/CPPSources/Utils/3dPart/BulletPhysics/LinearMath/btSerializer.o: \
- /Users/AreYouReal/Documents/Other/OMEngine/Platforms/Android/jni/../../../CPPSources/Utils/3dPart/BulletPhysics/LinearMath/btSerializer.cpp
